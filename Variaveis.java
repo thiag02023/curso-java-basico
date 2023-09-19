0b4fbb0357136956df8d707c331583574573a301
thiag02023 <132413991+thiag02023@users.noreply.github.com>
2023-09-19 17:54:53 -0300

    Add files via upload

---
+++ Variaveis.java
@@ -0,0 +1,10 @@
+package com.loiane.cursojava.aula10;
+
+public class Variaveis {
+    public static void main(String[] args){
+        int idade;
+        String nome;
+        String nomeDoMeuDog;
+
+    }
+}
